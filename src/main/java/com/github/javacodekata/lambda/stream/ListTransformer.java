bde7a5aa4fb0f86b3850e899e9f8c141c4478bf9
manojbajaj <manoj.bajaj@gmail.com>
2023-01-07 16:58:24 +0000

    List Transformer stream

--- src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
+++ src/main/java/com/github/javacodekata/lambda/stream/ListTransformer.java
@@ -2,56 +2,83 @@ package com.github.javacodekata.lambda.stream;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+
 public class ListTransformer {
 
-	private final List<String> values;
+    private final List<String> values;
 
-	private ListTransformer(List<String> values) {
-		this.values = values;
-	}
+    private ListTransformer(List<String> values) {
+        this.values = values;
+    }
 
-	public static ListTransformer of(List<String> values) {
-		checkNotNull(values);
-		return new ListTransformer(ImmutableList.copyOf(values));
-	}
+    public static ListTransformer of(List<String> values) {
+        checkNotNull(values);
+        return new ListTransformer(ImmutableList.copyOf(values));
+    }
 
-	/**
-	 * This method should take the String List and sort all the String elements in ascending (ASCII) order.
-	 * @return The sorted values in ascending ASCII order.
-	 */
-	public List<String> getSortedStrings() {
-		return values;
-	}
+    /**
+     * This method should take the String List and sort all the String elements in ascending (ASCII) order.
+     *
+     * @return The sorted values in ascending ASCII order.
+     */
+    public List<String> getSortedStrings() {
+        return values.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
-	/**
-	 * This method should take the String List and:
-	 * <ol>
-	 *     <li>filter the elements that contains one or more digits;</li>
-	 *     <li>transform (map) the remaining Strings into Integers;</li>
-	 *     <li>sort the Integers in ascending order.</li>
-	 * </ol>
-	 * @return
-	 */
-	public List<Integer> getSortedIntegers() {
-		return new LinkedList<>();
-	}
+    /**
+     * This method should take the String List and:
+     * <ol>
+     *     <li>filter the elements that contains one or more digits;</li>
+     *     <li>transform (map) the remaining Strings into Integers;</li>
+     *     <li>sort the Integers in ascending order.</li>
+     * </ol>
+     *
+     * @return
+     */
+    public List<Integer> getSortedIntegers() {
+        return values.stream()
+                .filter(value -> isInteger(value))
+                .map(value -> Integer.valueOf(value))
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
-	/**
-	 * This method should take the String List and:
-	 * <ol>
-	 *     <li>filter the elements that contains one or more digits;</li>
-	 *     <li>transform (map) the remaining Strings into Integers;</li>
-	 *     <li>sort the Integers in descending order.</li>
-	 * </ol>
-	 * @return
-	 */
-	public List<Integer> getSortedDescendingIntegers() {
-		return new LinkedList<>();
-	}
+    private boolean isInteger(String value) {
+        final int sz = value.length();
+        for (int i = 0; i < sz; i++) {
+            if (!Character.isDigit(value.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * This method should take the String List and:
+     * <ol>
+     *     <li>filter the elements that contains one or more digits;</li>
+     *     <li>transform (map) the remaining Strings into Integers;</li>
+     *     <li>sort the Integers in descending order.</li>
+     * </ol>
+     *
+     * @return
+     */
+    public List<Integer> getSortedDescendingIntegers() {
+        return values.stream()
+                .filter(value -> isInteger(value))
+                .map(value -> Integer.valueOf(value))
+                .sorted( Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
 
 }
